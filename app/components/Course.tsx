cb89c7ec57b07568506b15c1cc9be943041357f4
adairrohling <81537071+adairrohling@users.noreply.github.com>
2024-04-17 15:02:07 +0000

    feat: course e footer

---
+++ app/components/Course.tsx
@@ -0,0 +1,65 @@
+export default function Course(){
+    return (
+        <div className="mt-8 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/html.svg" alt="" />
+            <div>
+              <h4>CURSO DE HTML</h4>
+              <p>O curso de HTML é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+  
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/css.svg" alt="" />
+            <div>
+              <h4>CURSO DE CSS</h4>
+              <p>O curso de CSS é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+  
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/js.svg" alt="" />
+            <div>
+              <h4>CURSO DE JS</h4>
+              <p>O curso de JS é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+  
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/games.svg" alt="" />
+            <div>
+              <h4>CURSO DE GAMES</h4>
+              <p>O curso de GAMES é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+  
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/design.svg" alt="" />
+            <div>
+              <h4>CURSO DE DESIGN</h4>
+              <p>O curso de DESING é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+  
+        <div className="bg-[#4d4d4d] rounded-md lg:rounded-lg">
+          <a href="">
+            <img src="/img/robot.svg" alt="" />
+            <div>
+              <h4>CURSO DE ROBÓTICA</h4>
+              <p>O curso de Robótica é oferecido ....</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Banner from "./components/Banner";
+import Course from "./components/Course";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 export default function Home() {
   return (
     <div>
        <Header/>
        <Banner/>
+       <Course/>
+       <Footer/>
     </div>
   );
 }
