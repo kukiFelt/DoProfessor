1588807939b4b3077deb3777523cf079fc3d53e7
adairrohling <81537071+adairrohling@users.noreply.github.com>
2024-05-08 01:19:44 +0000

    feat: shadcn

--- app/admin/components/NavBar.tsx
+++ app/admin/components/NavBar.tsx
@@ -1,10 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function NavBar() {
     return (
         <div className="flex justify-between p-6 border-b-2">
             <h1>Logo</h1>
             <a href="/admin">Home</a>
             <a href="/admin/student">Navegar Estudantes</a>
-            <h1>Avatar</h1>
+            <Avatar>
+                <AvatarImage src="https://avatars.githubusercontent.com/u/81537071?v=4" alt="@shadcn" />
+                <AvatarFallback>AJR</AvatarFallback>
+            </Avatar>
         </div>
     )
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,16 @@
-export default function Admin(){
-    return(
-        <h1 className="text-center">Bem Vindo à Área Administrativa</h1>
+
+
+import { Button } from "@/components/ui/button";
+
+export default function Admin() {
+    return (
+        <div>
+            <h1 className="text-center">Bem Vindo à Área Administrativa</h1>
+            <Button>Salvar</Button>
+            <Button variant="destructive" >Excluir</Button>
+            <Button variant="secondary" >Novo</Button>
+           
+        </div>
+
     )
 }
